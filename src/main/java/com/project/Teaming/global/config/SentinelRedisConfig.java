7da7f5dce8828909cf64c23f68f1e242134a19ea
minseo <gwonm305@gmail.com>
2025-04-24 19:01:02 +0900

    refactor: 레디스 센티널 설정

--- src/main/java/com/project/Teaming/global/config/SentinelRedisConfig.java
+++ src/main/java/com/project/Teaming/global/config/SentinelRedisConfig.java
@@ -5,87 +5,77 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.project.Teaming.global.event.NotificationEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Profile("prod")
 @Configuration
 @EnableRedisRepositories
 @RequiredArgsConstructor
-public class RedisConfig {
+public class SentinelRedisConfig {
 
     private final RedisProperties redisProperties;
-    @Value("${spring.data.redis.host}") String host;
-    private final RedisConnectionFactory redisConnectionFactory;
 
     private RedisConnectionFactory redisConnectionFactory(int databaseIndex) {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
-        redisStandaloneConfiguration.setPort(redisProperties.getPort());
-        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
-        redisStandaloneConfiguration.setDatabase(databaseIndex);
-        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-        connectionFactory.afterPropertiesSet(); // 명시적으로 초기화
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration();
+        sentinelConfig.setMaster(redisProperties.getSentinel().getMaster());
+        Set<RedisNode> redisNodes = redisProperties.getSentinel().getNodes().stream()
+                .map(node -> {
+                    String[] parts = node.split(":");
+                    return new RedisNode(parts[0], Integer.parseInt(parts[1]));
+                })
+                .collect(Collectors.toSet());
+        sentinelConfig.setSentinels(redisNodes);
+        sentinelConfig.setPassword(redisProperties.getPassword());
+        sentinelConfig.setDatabase(databaseIndex);
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(sentinelConfig);
+        connectionFactory.afterPropertiesSet(); // 명시적 초기화
         return connectionFactory;
     }
 
-    // 기본 db(0번)용 RedisTemplate
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
-        // 0번 DB(기본값) 설정
-        // redisTemplate 를 받아와서 set, get, delete 를 사용
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        /*
-         * setKeySerializer, setValueSerializer 설정
-         * redis-cli 을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-         */
         redisTemplate.setConnectionFactory(redisConnectionFactory(0));
-
         return redisTemplate;
     }
 
-
-    // 게시글 db(1번)용 RedisTemplate
     @Bean
     public RedisTemplate<String, Object> postRedisTemplate() {
-        // 1번 DB에서 사용할 RedisTemplate 설정
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory(1));
-
-        // 직렬화
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
         return template;
     }
-    // 강퇴, 탈퇴한 팀원 및 지원자 db(2번) 용 RedisTemplate
+
     @Bean
     public RedisTemplate<String, Object> participationRedisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory(2));
-        // JSON 직렬화 설정
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper());
-        template.setKeySerializer(new StringRedisSerializer()); // 키 직렬화
-        template.setValueSerializer(serializer);               // 값 직렬화
-        template.setHashKeySerializer(new StringRedisSerializer()); // 해시 키 직렬화
-        template.setHashValueSerializer(serializer);                // 해시 값 직렬화
-
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
         return template;
     }
 
-    // sse 연결한 사용자 공유용
     @Bean
     public StringRedisTemplate stringRedisTemplate() {
         StringRedisTemplate template = new StringRedisTemplate();
@@ -98,8 +88,6 @@ public class RedisConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        // Redis에서만 사용할 타입 정보 활성화
         objectMapper.activateDefaultTyping(
                 BasicPolymorphicTypeValidator.builder()
                         .allowIfSubType(Object.class)
@@ -107,7 +95,7 @@ public class RedisConfig {
                 ObjectMapper.DefaultTyping.NON_FINAL,
                 JsonTypeInfo.As.PROPERTY
         );
-
         return objectMapper;
     }
 }
+
